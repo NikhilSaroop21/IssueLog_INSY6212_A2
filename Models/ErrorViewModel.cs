5686bf253be52e03d14c355dffce6af8e1ea39b5
NikhilSaroop21 <128732882+NikhilSaroop21@users.noreply.github.com>
2025-05-08 14:18:55 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IssueLog_INSY6212_A2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
